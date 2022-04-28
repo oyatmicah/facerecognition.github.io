334046905d8ac5b4593f121809a9377776cc48c8
oyatmicah <oyatmicah@gmail.com>
2022-04-28 10:31:14 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -29,20 +29,22 @@ class App extends Component {
         name: "",
         email: "",
         entries: 0,
-        joined: ""
+        joined: "",
       },
     };
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
+    this.setState({
+      user: {
+        id: data.id,
         name: data.name,
         email: data.email,
         entries: data.entries,
-        joined: data.joined
-    }})
-  }
+        joined: data.joined,
+      },
+    });
+  };
 
   onInputChange = (event) => {
     console.log({ input: event.target.value });
@@ -205,7 +207,10 @@ class App extends Component {
           route === "signin" ? (
             <SignIn onRouteChange={this.onRouteChange} />
           ) : (
-            <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            <Register
+              loadUser={this.loadUser}
+              onRouteChange={this.onRouteChange}
+            />
           ))
         )}
       </div>
